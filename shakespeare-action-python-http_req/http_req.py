e46a5aaae6a8e19a696bfe28ddb3a2f40e5e3465
william@雾帜 <william.xu@wuzhi-ai.com>
2024-01-04 14:55:15 +0800

    update:更新http_req的header处理
    update:更新http_req的header处理

--- shakespeare-action-python-http_req/http_req.py
+++ shakespeare-action-python-http_req/http_req.py
@@ -9,7 +9,9 @@ def http_requests(params, assets, context_info):
     # HTTP请求的url
     url = params["url"]
     # HTTP请求的请求头
-    headers = params.get("headers", HEADERS.NONE)
+    headers=""
+    if "headers" in params.keys() :
+        headers = params.get("headers", HEADERS.NONE)
     # HTTP请求的SSL验证
     verify = params.get("ssl_verify", False)
     # HTTP请求的请求体
@@ -24,23 +26,27 @@ def http_requests(params, assets, context_info):
     
     '''
     h = HTTP()
-    if type(headers) == str:
-        headers = headers.replace("'", '"')
-        x = str_to_json(headers)
-        print(type(x))
-        h.headers = x
-    elif type(headers) == dict:
-        h.headers = headers
-    else:
-        json_ret['msg'] = "headers 无效, 请检查headers。"
-    h.verify = verify
-    if verify_json(data):
-        json_ret = h.req(method, url=url, body=None, json_data=data)
-    else:
-        json_ret = h.req(method, url=url, body=data, json_data=None)
+    try:
+        if type(headers) == str and headers is not None and headers !='':
+            headers = headers.replace("'", '"')
+            x = str_to_json(headers)
+            print(type(x))
+            h.headers = x
+        elif type(headers) == dict:
+            h.headers = headers
+        else:
+            h.headers=None
+        h.verify = verify
+        if verify_json(data):
+            json_ret = h.req(method, url=url, body=None, json_data=data)
+        else:
+            json_ret = h.req(method, url=url, body=data, json_data=None)
 
-    del h
-    if json_ret.get("data",{}).get("content", None) is not None:
-        del json_ret['data']['content']
-
+        del h
+        if json_ret.get("data",{}).get("content", None) is not None:
+            del json_ret['data']['content']
+    except Exception as e:
+        json_ret={
+            "code": 500, "msg": str(e)
+        }
     return json_ret 
